cb8ea4050bdfcd16773df4e1808d9b11a645d3de
Dev-syphax <email.syphax@gmail.com>
2026-01-08 19:30:36 +0100

    add transaction management utility for database operations

---
+++ db/transactions.js
@@ -0,0 +1,18 @@
+const pool = require('./index');
+
+async function runTransaction(callback) {
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await callback(client);
+        await client.query('COMMIT');
+        return result;
+    } catch (error) {
+        await client.query('ROLLBACK');
+        throw error;
+    } finally {
+        client.release();
+    }
+}
+
+module.exports = runTransaction;
