2babcf836943ac530ea3b21ab53db3e22a0875c2
Dev-syphax <email.syphax@gmail.com>
2026-01-09 00:30:17 +0100

    add CORS and crypto dependencies to package.json and package-lock.json; update app.js to use CORS middleware

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const paymentRoutes = require('./modules/payment.routes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Payment Gateway API is running');
 });
 
+app.use('/api/payments', paymentRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
